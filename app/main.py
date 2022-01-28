f08cf401cf983c7f793b9ad5d703bd973673b047
Vyacheslav Kuzmin <vyvkuzmin@greenatom.ru>
2022-01-28 07:29:57 +0300

    + HTTP

--- app/main.py
+++ app/main.py
@@ -1,17 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
+from fastapi.responses import HTMLResponse
+
 
 import model
 
 app = FastAPI(title="Bender's mouth")
 
 
-@app.post("/answer",
-          response_description="Bender's answer",
-          description="Get Bender's answer")
-def answer(data: model.PhraseInput):
+@app.get("/answer",
+         response_description="Bender's answer",
+         description="Get Bender's answer")
+async def answer(data: model.PhraseInput):
     return {"phrase": data.phrase}
 
 
-@app.get("/")
-def main():
-    return "main"
+@app.get("/answer",
+         response_description="Bender's answer",
+         description="Get Bender's answer")
+async def answer_form(phrase: str = Form(...)):
+    return {"phrase": phrase}
+
+
+@app.get("/", response_class=HTMLResponse)
+async def main():
+    html_content = """
+    <html>
+        <head>
+            <title>Some HTML in here</title>
+        </head>
+        <body>
+            <h1>Look ma! HTML!</h1>
+        </body>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
