4e6112ae4d17d6fcf76749c7c1cb5cfa4017cf8e
Vyacheslav Kuzmin <vyvkuzmin@greenatom.ru>
2022-01-29 21:20:47 +0300

    Test api

--- webapp/model.py
+++ webapp/model.py
@@ -1,10 +1,5 @@
-import typing
 from pydantic import BaseModel, Field
 
 
 class PhraseInput(BaseModel):
     phrase: str = Field(title="Text", description="Text of phrase", max_lengh=200)
-
-
-class PhraseOutput(BaseModel):
-    phrase: typing.Optional[str] = None
