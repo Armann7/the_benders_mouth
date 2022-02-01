246de993c0d935809bc9fc7c21e8fca7034caec3
Vyacheslav Kuzmin <vyvkuzmin@greenatom.ru>
2022-02-01 22:25:39 +0300

    #run_pipe Переход на асинхронное выполнение запросов к сетке

--- webapp/main.py
+++ webapp/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
+import asyncio
 
 from app.conversation import Conversation
 import webapp.model
@@ -10,13 +11,26 @@ app = FastAPI(title="Bender's mouth")
 templates = Jinja2Templates(directory=r"data/templates")
 
 
+async def get_answer(phrase: str) -> str:
+    """
+    Блокирующий запрос внутри асинхронного
+    :param phrase:
+    :return:
+    """
+    loop = asyncio.get_event_loop()
+    future_request = loop.run_in_executor(None, Conversation().answer, phrase)
+    text = await future_request
+    return text
+
+
 # API
 #
 @app.post("/api/answer",
           response_description="Bender's answer",
           description="Get Bender's answer")
 async def answer(data: webapp.model.PhraseInput):
-    return {"answer": Conversation().answer(data.phrase)}
+    text = await get_answer(data.phrase)
+    return {"answer": text}
 
 
 # Web interface
@@ -32,7 +46,7 @@ async def favicon():
           response_description="Bender's answer",
           description="Get Bender's answer")
 async def answer_form(phrase: str = Form(...)):
-    Conversation().answer(phrase)
+    await get_answer(phrase)
     return RedirectResponse("/")
 
 
